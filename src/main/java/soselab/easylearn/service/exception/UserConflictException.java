9b350c7cb50d1333b12b83c3a09935543e6e6556
Bernie <b4456609@gmail.com>
2016-10-23 02:22:36 +0800

    add run time exception

--- src/main/java/soselab/easylearn/service/exception/UserConflictException.java
+++ src/main/java/soselab/easylearn/service/exception/UserConflictException.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Created by bernie on 2016/9/10.
  */
 @ResponseStatus(value = HttpStatus.CONFLICT, reason = "User conflict")
-public class UserConflictException {
+public class UserConflictException extends RuntimeException {
 }
